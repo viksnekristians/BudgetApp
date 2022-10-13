ea0582b81cc2c195b791c546f70ad897d914db64
viksnekristians <viksnekristians@gmail.com>
2022-10-13 17:08:01 +0300

    change all expenses view functionality from dropdown to multiple categories buttons

---
+++ budget-frontend/src/components/CategoryButton.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function CategoryButton({text, onClick}) {
+  const [isActive, setIsActive] = useState(false)
+  const handleClick = () => {
+    onClick();
+    setIsActive(!isActive);
+  }
+  return (
+    <a className='category-btn' style={ isActive ? { backgroundColor:'#1363DF', color:'white'} : {}} onClick={handleClick}>{text}</a>
+  )
+}
+
+export default CategoryButton
